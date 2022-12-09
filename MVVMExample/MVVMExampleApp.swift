a4623bf78a036ced12fec1749980603aa749ca5d
Rodolfo Gutierrez <rgutierrez@trackchain.io>
2022-12-09 10:07:42 -0600

    first commit

---
+++ MVVMExample/MVVMExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMExampleApp.swift
+//  MVVMExample
+//
+//  Created by Rodolfo Gutierrez on 9/12/22.
+//
+
+import SwiftUI
+
+@main
+struct MVVMExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
